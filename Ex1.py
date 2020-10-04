6df5ca482e88f6ea4c395287756e4672dc35f098
Eliott <eliott.vdsw@yahoo.fr>
2020-10-05 01:35:24 +0200

    Add Gaussian distribution

--- Ex1.py
+++ Ex1.py
@@ -3,16 +3,7 @@ import numpy as np
 import random
 
 vec = []
-i = 0
-while i < 50:
-    error = random.uniform(0,2)
-    if random.randint(0, 1) % 2 == 0 :
-        vec.append(100-error)
-    else:
-        vec.append(100+error)
-    i += 1
-
-print(vec)
+vec.append(np.random.normal(100, 0.1,5000))
  
-plt.hist(vec,25)          
+plt.hist(vec,50)          
 plt.show()                  
